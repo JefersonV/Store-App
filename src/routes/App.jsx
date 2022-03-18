6d3463d91c797f30e6c373d9bd2b7c6f35344520
Jeferson <jvelasquezc15@miumg.edu.gt>
2022-03-18 02:50:08 -0600

    05 - Reorganización de carpetas -> <routes> (App) y <containers> (layout)

---
+++ src/routes/App.jsx
@@ -0,0 +1,24 @@
+import '../sass/index.scss'
+//components
+import RecoverPassword from '../components/RecoverPassword'
+import LoginCheck from '../components/LoginCheck'
+import Login from '../components/Login'
+import LoginRegister from '../components/LoginRegister'
+import Layout from '../containers/Layout'
+//Svg
+// import logoYard from './assets/logos/logo_yard_sale.svg'
+function App() {
+
+  return (
+    <>
+    <Layout >
+      <RecoverPassword />
+      <LoginCheck />
+      <Login />
+      <LoginRegister />
+    </Layout>
+    </>
+  )
+}
+
+export default App
