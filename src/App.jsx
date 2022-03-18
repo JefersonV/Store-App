340b38963e43551c4ffbaa9743708930a92caeb3
Jeferson <jvelasquezc15@miumg.edu.gt>
2022-03-18 02:14:05 -0600

    03 - componente loginRegister y Layout(props.children) agregado previo a REACT ROUTER

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import './sass/index.scss'
 import RecoverPassword from './components/RecoverPassword'
 import LoginCheck from './components/LoginCheck'
 import Login from './components/Login'
+import LoginRegister from './components/LoginRegister'
+import Layout from './components/Layout'
 //Svg
 // import logoYard from './assets/logos/logo_yard_sale.svg'
 function App() {
 
   return (
     <>
+    <Layout >
       <RecoverPassword />
       <LoginCheck />
       <Login />
+      <LoginRegister />
+    </Layout>
     </>
   )
 }
