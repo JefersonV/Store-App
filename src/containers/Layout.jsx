6147e68262c0f6e8380f45b7516f13e6fc10d35c
Jeferson <jvelasquezc15@miumg.edu.gt>
2022-03-18 03:31:57 -0600

    06 - React Router DOM V6 listo -> hay que realizar los componentes NotFound y Home

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Layout = (props) => {
+const Layout = ({ children }) => {
   return (
     <div className="Layout">
-      {props.children}
+      { children }
     </div>
   )
 }
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,23 +1,30 @@
 import '../sass/index.scss'
+//Router
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 //components
 import RecoverPassword from '../components/RecoverPassword'
 import LoginCheck from '../components/LoginCheck'
 import Login from '../components/Login'
 import LoginRegister from '../components/LoginRegister'
 import Layout from '../containers/Layout'
+import Home from '../components/Home'
+import NotFound from '../components/NotFound'
+
 //Svg
 // import logoYard from './assets/logos/logo_yard_sale.svg'
 function App() {
 
   return (
-    <>
-    <Layout >
-      <RecoverPassword />
-      <LoginCheck />
-      <Login />
-      <LoginRegister />
-    </Layout>
-    </>
+      <BrowserRouter>
+        <Layout >
+          <Routes> 
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/recover" element={<RecoverPassword />} />
+              <Route path="*"element={ <NotFound /> } />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
   )
 }
 
